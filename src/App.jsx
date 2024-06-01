b05b2cc32b4126fbc8ca5074eac68e9de74e5070
Jinnie <hjk7639@gmail.com>
2024-06-01 23:11:40 +0900

    Add: UI Structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>JINNIEVE🎬</h1>
+      </header>
+      <main>
+        <section>
+          <ul>
+            <li>
+              <button>movie</button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2></h2>
+          <img src="" alt="" />
+          <p></p>
+        </section>
+      </main>
+      <footer>api credit: Nomad Coder</footer>
+    </>
+  );
 }
 
 export default App;
